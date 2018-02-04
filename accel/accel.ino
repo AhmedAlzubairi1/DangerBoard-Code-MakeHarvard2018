18583216feb38e723b7ce816f3f224379f1c8093
Nick Piscitello <npiscitello@users.noreply.github.com>
2018-02-04 11:55:35 -0500

    Arduino interfacing

--- accel/accel.ino
+++ accel/accel.ino
@@ -7,12 +7,17 @@ float rawz = 0;
 void setup()
 {
     accel_setup();
+    pinMode(4, OUTPUT);
     Serial.begin(115200);
 }
 
 void loop()
 {
-  Serial.print("speed: ");
-  Serial.println(get_accel());
+  float accel = get_accel();
+  if( accel < 0.3 && accel > -0.3 ) {
+    digitalWrite(4, LOW);
+  } else {
+    digitalWrite(4, HIGH);
+  }
   delay(PERIOD_MS);
 }
