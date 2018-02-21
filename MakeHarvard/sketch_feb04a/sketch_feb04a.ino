352dffe388fa510762fc7a9e360a88ed393e2acd
AhmedAlzubairi1 <34204200+AhmedAlzubairi1@users.noreply.github.com>
2018-02-21 13:15:46 -0500

    Add files via upload
    I created the main code of the board that utilized the led.h file that my teammate created. My portion of the code is the portion that controls how exactly and when the LED strips react.

---
+++ MakeHarvard/sketch_feb04a/sketch_feb04a.ino
@@ -0,0 +1,26 @@
+int trigPinRight=9;
+int echoPinRight=10;
+int n =10;
+int timeRight;
+int distanceRight;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(trigPinRight,OUTPUT);
+pinMode(echoPinRight,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  digitalWrite(trigPinRight,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinRight,HIGH);
+  delayMicroseconds(n);
+  digitalWrite(trigPinRight,LOW);
+  timeRight=pulseIn(echoPinRight,HIGH);
+  distanceRight=timeRight*.034/2;
+  Serial.println(distanceRight);
+  delay(1000);
+}
---
+++ MakeHarvard/sketch_feb04a/sketch_feb04a/sketch_feb04a.ino
@@ -0,0 +1,22 @@
+int trigPinRight=4;
+int echoPinRight=5;
+int n=10;
+int timeRight;
+int distanceRight;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(echoPinRight,INPUT);
+pinMode(trigPinRight,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(trigPinRight,HIGH);
+  delayMicroseconds(n);
+  digitalWrite(trigPinRight,LOW);
+  timeRight=pulseIn(echoPinRight,HIGH);
+  distanceRight=timeRight*.034/2;
+  Serial.println(distanceRight);
+  delay(50);
+}
